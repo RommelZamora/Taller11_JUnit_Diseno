70da6317e3221ca0efbb042ee8515cbac2c1cad4
RommelZamora <razamora@espol.edu.ec>
2023-08-21 17:08:41 -0500

    Arreglos pomxl

--- TallerPruebasUnitarias/src/main/java/org/example/Main.java
+++ TallerPruebasUnitarias/src/main/java/org/example/Main.java
@@ -2,5 +2,9 @@ package org.example;
 
 public class Main {
     public static void main(String[] args) {
+        
+        //Calculadora calc = new Calculadora();
+        //System.out.println(calc.radicacion(16, 2));
+        
     }
 }
---
+++ TallerPruebasUnitarias/src/test/java/RadicacionTest.java
@@ -0,0 +1,47 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/UnitTests/JUnit5TestClass.java to edit this template
+ */
+
+import org.example.Calculadora;
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.AfterAll;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.*;
+
+/**
+ *
+ * @author radri
+ */
+public class RadicacionTest {
+    
+    public RadicacionTest() {
+    }
+    
+    @BeforeAll
+    public static void setUpClass() {
+    }
+    
+    @AfterAll
+    public static void tearDownClass() {
+    }
+    
+    @BeforeEach
+    public void setUp() {
+    }
+    
+    @AfterEach
+    public void tearDown() {
+    }
+
+    // TODO add test methods here.
+    // The methods must be annotated with annotation @Test. For example:
+    //
+    @Test
+    public void radicacionTest() {
+        Calculadora calc = new Calculadora();
+        assertEquals(6,calc.radicacion(16,2));
+    }
+}
